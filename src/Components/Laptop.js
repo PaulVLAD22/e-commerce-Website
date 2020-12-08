fdfe5580cd6295f11eb549e5f8d67f94082d5787
PaulVlad22 <paulvlad34@gmail.com>
2020-12-08 14:41:50 +0200

    .

--- src/Components/Laptop.js
+++ src/Components/Laptop.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const Laptop = ({id,img,descr,brand,cpu,storage,gpu,price})=>{
+const Laptop = ({id,img,name,brand,cpu,storage,gpu,price})=>{
   return (
     <div className="container-product d-flex flex-column align-items-center justify-content-center col-4 col-xl-2 col-lg-3 col-md-4 col-sm-4 " id={id}>
        <picture className="container container-img ">
          <img src={img} className="img-fluid img-thumbnail"></img>
         </picture>
-        <h2 className='product-descr  text-justify'>{descr+" - "+price}</h2>
+        <h2 className='product-descr  text-justify'>{name+" - "+price}</h2>
         <ul className="laptop-comp">
           <li>CPU: {cpu}</li>
           <li>Storage: {storage}</li>
--- src/data/smartphones.js
+++ src/data/smartphones.js
@@ -2,91 +2,12 @@ const smartphonesRaw=[
   {
     id:1,
     img:"https://lcdn.altex.ro/resize/media/catalog/product/S/M/16fa6a9aef7ffd6209d5fd9338ffa0b1/SMTA20SBK_2.jpg",
-    descr:"Hamster Galaxy S8, 2GB RAM, 64 GB Storage",
+    descr:"2GB RAM, 64 GB Storage",
+    name:"Hamster Galaxy S8",
     brand:"Hamster",
     price:"100$"
-
-  },
-  {
-    id:1,
-    img:"https://lcdn.altex.ro/resize/media/catalog/product/S/M/16fa6a9aef7ffd6209d5fd9338ffa0b1/SMTA20SBK_2.jpg",
-    descr:"Hamster Galaxy S8, 2GB RAM, 64 GB Storage",
-    brand:"Hamster",
-    price:"100$"
-
-  },
-  {
-    id:1,
-    img:"https://lcdn.altex.ro/resize/media/catalog/product/S/M/16fa6a9aef7ffd6209d5fd9338ffa0b1/SMTA20SBK_2.jpg",
-    descr:"Hamster Galaxy S8, 2GB RAM, 64 GB Storage",
-    brand:"Hamster",
-    price:"100$"
-
-  },
-  {
-    id:1,
-    img:"https://lcdn.altex.ro/resize/media/catalog/product/S/M/16fa6a9aef7ffd6209d5fd9338ffa0b1/SMTA20SBK_2.jpg",
-    descr:"Hamster Galaxy S8, 2GB RAM, 64 GB Storage",
-    brand:"Hamster",
-    price:"100$"
-
-  },{
-    id:1,
-    img:"https://lcdn.altex.ro/resize/media/catalog/product/S/M/16fa6a9aef7ffd6209d5fd9338ffa0b1/SMTA20SBK_2.jpg",
-    descr:"Hamster Galaxy S8, 2GB RAM, 64 GB Storage",
-    brand:"Hamster",
-    price:"100$"
-
   }
-  ,{
-    id:1,
-    img:"https://lcdn.altex.ro/resize/media/catalog/product/S/M/16fa6a9aef7ffd6209d5fd9338ffa0b1/SMTA20SBK_2.jpg",
-    descr:"Hamster Galaxy S8, 2GB RAM, 64 GB Storage",
-    brand:"Hamster",
-    price:"100$"
 
-  }
-  ,{
-    id:1,
-    img:"https://lcdn.altex.ro/resize/media/catalog/product/S/M/16fa6a9aef7ffd6209d5fd9338ffa0b1/SMTA20SBK_2.jpg",
-    descr:"Hamster Galaxy S8, 2GB RAM, 64 GB Storage",
-    brand:"Hamster",
-    price:"100$"
-
-  }
-  ,{
-    id:1,
-    img:"https://lcdn.altex.ro/resize/media/catalog/product/S/M/16fa6a9aef7ffd6209d5fd9338ffa0b1/SMTA20SBK_2.jpg",
-    descr:"Hamster Galaxy S8, 2GB RAM, 64 GB Storage",
-    brand:"Hamster",
-    price:"100$"
-
-  }
-  ,{
-    id:1,
-    img:"https://lcdn.altex.ro/resize/media/catalog/product/S/M/16fa6a9aef7ffd6209d5fd9338ffa0b1/SMTA20SBK_2.jpg",
-    descr:"Hamster Galaxy S8, 2GB RAM, 64 GB Storage",
-    brand:"Hamster",
-    price:"100$"
-
-  }
-  ,{
-    id:1,
-    img:"https://lcdn.altex.ro/resize/media/catalog/product/S/M/16fa6a9aef7ffd6209d5fd9338ffa0b1/SMTA20SBK_2.jpg",
-    descr:"Hamster Galaxy S8, 2GB RAM, 64 GB Storage",
-    brand:"Hamster",
-    price:"100$"
-
-  }
-  ,{
-    id:1,
-    img:"https://lcdn.altex.ro/resize/media/catalog/product/S/M/16fa6a9aef7ffd6209d5fd9338ffa0b1/SMTA20SBK_2.jpg",
-    descr:"Hamster Galaxy S8, 2GB RAM, 64 GB Storage",
-    brand:"Hamster",
-    price:"100$"
-
-  }
-  
 ]
 // dividing them in lines of size
 export const smartphones =[],size=4;
